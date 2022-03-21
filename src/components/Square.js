9ae4e8ef1af24fda819ae90704b65f177b5fd85c
Zhuoyi Li <joeyzli@berkeley.edu>
2022-03-20 18:37:40 -0700

    done

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,12 +1,12 @@
-import React from "react";
+import React from 'react';
 import './styles/Square.css';
 
-function Square() {
-    return (
-        <button className="square">
-            {/* TODO */}
-        </button>
-    )
+function Square({ mySymbol, myUpdater }) {
+  return (
+    <button className="square" onClick={(e) => myUpdater()}>
+      {mySymbol}
+    </button>
+  );
 }
 
 export default Square;
